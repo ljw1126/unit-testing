65e577c5275d078b20c55cb11998ab67fe1cb36b
이진우 <leejinwoo1126@gmail.com>
2025-09-23 21:28:18 +0900

    init

---
+++ README.md
@@ -0,0 +1,14 @@
+
+**초기화**
+프로젝트 이름을 명시하지 않으면 부모 디렉터리명을 따라감
+```shell
+> mkdir unit-testing
+> dotnet new xunit 
+```
+
+
+**추가 익스텐션**
+- C# Dev Kit
+- C#
+
+> Test Explorer UI, .Net Core Test Explorer를 설치안해도 C# Dev Kit 통해 TESTING 지원함
---
+++ UnitTest1.cs
@@ -0,0 +1,21 @@
+﻿namespace unit_testing;
+
+public class UnitTest1
+{
+    [Fact(DisplayName = "덧셈")]
+    public void Test1()
+    {
+        Assert.Equal(4, Sum(2, 2));
+    }
+
+    [Fact(DisplayName = "문자열 비교")]
+    public void Test2()
+    {
+        Assert.Equal("123", "123");
+    }
+
+    private int Sum(int a, int b)
+    {
+        return a + b;
+    }
+}
