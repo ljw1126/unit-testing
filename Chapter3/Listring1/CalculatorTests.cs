a3d3acef103dd0409a49dcace17061cc0b5b3560
이진우 <leejinwoo1126@gmail.com>
2025-09-26 17:00:03 +0900

    예제 3.6 모든 테스트에 공유하는 준비 및 종료 로직

--- Chapter3/Listring1/CalculatorTests.cs
+++ Chapter3/Listring1/CalculatorTests.cs
@@ -2,6 +2,7 @@ namespace unit_testing.Chapter3.Listing1
 {
     public class CalculatorTests
     {
+        // 예제 3.4 ~ 3.5
         [Fact]
         public void Sum_of_two_number()
         {
@@ -23,6 +24,42 @@ namespace unit_testing.Chapter3.Listing1
         public double Sum(double first, double second)
         {
             return first + second;
+        }
+
+        public void CleanUp()
+        {
+        }
+    }
+
+    public class CalculatorTest2 : IDisposable
+    {
+        private readonly Calculator sut;
+
+        // 클래스 내 각 테스트 이전에 호출
+        public CalculatorTest2()
+        {
+            sut = new Calculator();
+        }
+
+        // 클래스 내 각 테스트 이후에 호출
+        public void Dispose()
+        {
+            sut.CleanUp();
+        }
+
+        [Fact]
+        public void Sum_of_two_number()
+        {
+            // Arrange
+            double first = 10;
+            double second = 20;
+            var sut = new Calculator();
+
+            // Act
+            double actual = sut.Sum(first, second);
+
+            // Assert
+            Assert.Equal(30, actual);
         }
     }
 }
